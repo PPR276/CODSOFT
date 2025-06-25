6496ad60862c505380a28ebab0d074e349c3f3da
PAMULA PRAVALLIKA REDDY <pamulapravallikareddy@gmail.com>
2025-06-25 20:13:13 +0530

    code

---
+++ CALCULATOR.py
@@ -0,0 +1,11 @@
+a = int(input("enter the first number : "))
+b = int(input("enter the second number : "))
+c=input("enter the operation you wish to perform among '+' , '-' , 'x' , '/' : ")
+if c=="+":
+    print(a+b)
+elif c=="-":
+    print(a-b)
+elif c=="x":
+    print(a*b)
+else:
+    print(a/b)
